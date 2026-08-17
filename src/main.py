dd29978582507e24c92d8add8bc795cb1eb56af3
Tyler <tyler2ber@gmail.com>
2026-08-17 15:21:52 -0400

    Renamed dictionary 'regions_nation', added CALCULATE and PRINT w/ all_reached_300million()

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # data
-regions_nation = {
+nation_populations = {
     "Canada": 41417056,
     "Australia": 27262902,
     "New Zealand": 5361300,
     "UK": 70011675
 }
-regions_nation_total = sum(regions_nation.values())
+nation_populations_total = sum(nation_populations.values())
 
 # projections data w/ df_projections
 projections = {
@@ -38,7 +38,47 @@ df_projections = pd.DataFrame(columns=
     }
 )
 
-print(df_projections.head())
-
 # PROJECTIONS
-# ..
+
+def all_reached_300million():
+    for projection in projections:
+        if projections[projection] < 300000000:
+            return False
+    return True
+
+print("\nPROJECTIONS")
+
+gen_counter = 0
+while not all_reached_300million():
+
+    # CALCULATE
+    if projections_total == -1:
+
+        # gen (init)
+        gen_counter = 1
+
+        # projections (init)
+        for projection in projections:
+            projections[projection] = nation_populations[projection]
+
+        # projections total (init)
+        projections_total = nation_populations_total
+
+    else:
+
+        # gen
+        gen_counter += 1
+
+        # projections
+        for projection in projections:
+            projections[projection] = (projections[projection] / 2) * 3
+
+        # projections total
+        projections_total = sum(projections.values())
+
+    # PRINT
+    print(f"====> canzuk_nations(gen{gen_counter}): {round(projections_total):,}")
+    for projection in projections:
+        print(f"{'✔ ' if projections[projection] >= 300000000 else '- '} {round(projections[projection]):,} in {projection}")
+
+    # ...and update df_projections
